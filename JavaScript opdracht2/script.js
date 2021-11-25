de06405b22c37a86b0b50967f02b7b8ed9f7b3f6
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-11-25 08:17:26 +0100

    Add files via upload

---
+++ JavaScript opdracht2/script.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy Students");
+// This is a large sum.
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);
